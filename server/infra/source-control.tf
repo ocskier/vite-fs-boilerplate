3a844e6cf422402449b5956e4ae95849a1497695
Jon Jackson <ocskier@gmail.com>
2024-12-15 19:42:13 -0500

    Removes source control for manual

--- server/infra/source-control.tf
+++ server/infra/source-control.tf
@@ -1,8 +1,8 @@
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id                 = azurerm_linux_web_app.vite_fs_boilerplate.id
-  repo_url               = "https://github.com/ocskier/vite-fs-boilerplate"
-  branch                 = "main"
-  use_manual_integration = true
+# resource "azurerm_app_service_source_control" "sourcecontrol" {
+#   app_id                 = azurerm_linux_web_app.vite_fs_boilerplate.id
+#   repo_url               = "https://github.com/ocskier/vite-fs-boilerplate"
+#   branch                 = "main"
+#   use_manual_integration = true
 
-  depends_on = [azurerm_linux_web_app.vite_fs_boilerplate]
-}
+#   depends_on = [azurerm_linux_web_app.vite_fs_boilerplate]
+# }
